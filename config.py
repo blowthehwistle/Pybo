846b398b764c4113472d355f954490031ef644a5
easthwi <xjkkm5918@naver.com>
2021-08-04 00:32:23 +0900

    day4

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))  #database 접속 주소
 SQLALCHEMY_TRACK_MODIFICATIONS = False #이벤트 처리 옵션
+
+SECRET_KEY = "dev"
